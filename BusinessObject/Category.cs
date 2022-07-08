75e4a74c5aa0adcef8e2af79418c4d0b2cdca4cf
ohlalata <nguyenbaotoan.8345@gmail.com>
2022-07-08 22:20:57 +0700

    Product and Category

--- BusinessObject/Category.cs
+++ BusinessObject/Category.cs
@@ -2,5 +2,14 @@
 {
     public class Category
     {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
