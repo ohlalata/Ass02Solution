c57a751f312711964149c36768e3ac36d003a26f
Minh-Trung <nguyenminhtrungse@gmail.com>
2022-07-08 23:11:43 +0700

    Implement CRUD Order

--- DataAccess/Repository/OrderRepository/IOrderRepository.cs
+++ DataAccess/Repository/OrderRepository/IOrderRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace DataAccess.Repository.OrderRepository
+﻿using BusinessObject;
+
+namespace DataAccess.Repository.OrderRepository
 {
     public interface IOrderRepository
     {
+        public Order AddOrder(Order order);
+        public IEnumerable<Order> GetAllOrder();
+        public IEnumerable<Order> GetAllOrderFilterByDate(DateTime? startDate, DateTime? endDate);
+        public void DeleteOrder(int orderId);
+        public void DeleteByMember(int memberId);
     }
 }
