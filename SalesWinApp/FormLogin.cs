7683d073da6f115094197a0840fd912c8966fb79
Minh-Trung <nguyenminhtrungse@gmail.com>
2022-07-09 00:39:16 +0700

    Add UI and handle event FormProductManagement, LoginFrom, MainForm

--- SalesWinApp/FormLogin.cs
+++ SalesWinApp/FormLogin.cs
@@ -1,20 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using AutoMapper;
+using BusinessObject;
+using DataAccess.Repository.CartRepository;
+using DataAccess.Repository.MemberRepository;
+using SalesWinApp.Presenter;
+using SalesWinApp.ProductUI;
 
 namespace SalesWinApp
 {
     public partial class FormLogin : Form
     {
+        private readonly IMemberRepository memberRepository = new MemberRepository();
+        private readonly ICartRepository cartRepository = new CartRepository();
+        private readonly IMapper mapper;
+        private bool isAdminLogin;
+        private bool isMemberLogin;
         public FormLogin()
         {
             InitializeComponent();
+            mapper = new MapperConfiguration(
+                (config) =>
+                {
+                    config.AddProfile(new MappingProfileConfiguration());
+                }
+            ).CreateMapper();
+        }
+
+        private void CheckLogin()
+        {
+            try
+            {
+                string loginEmail = textBoxEmail.Text;
+                string loginPassword = txtPassword.Text;
+
+                isAdminLogin = memberRepository.IsAdminLogin(loginEmail, loginPassword);
+                if (isAdminLogin)
+                {
+                    FormMain frmMain = new FormMain
+                    {
+                        IsAdminLogin = this.isAdminLogin,
+                        MemberRepository = memberRepository,
+                        CartRepository = this.cartRepository
+                    };
+                    frmMain.Closed += (_, _) => this.Close();
+                    Hide();
+                    frmMain.Show();
+                    return;
+                }
+
+                Member member = memberRepository.Login(loginEmail, loginPassword);
+                isMemberLogin = member != null;
+                if (isMemberLogin)
+                {
+                    MemberPresenter memberLogin = mapper.Map<MemberPresenter>(member);
+                    FormProductsManagement formProductsManagement = new FormProductsManagement()
+                    {
+                        MemberLogin = memberLogin,
+                        IsMemberLogin = true,
+                        MemberRepository = memberRepository,
+                        CartRepository = this.cartRepository
+                    };
+                    formProductsManagement.Closed += (_, _) => this.Close();
+                    this.Hide();
+                    formProductsManagement.Show();
+                    return;
+                }
+
+
+                if (MessageBox.Show(@"Login failed email or password not correct!", @"Login", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Cancel)
+                {
+                    Close();
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"System busy can't login. Try again!", @"Login failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            CheckLogin();
+        }
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
