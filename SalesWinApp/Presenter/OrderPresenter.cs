2e2ed37e7645ebed4993ce0c8e3cb9a3f0244ab0
Minh-Trung <nguyenminhtrungse@gmail.com>
2022-07-08 23:49:19 +0700

    Add config mapper

--- SalesWinApp/Presenter/OrderPresenter.cs
+++ SalesWinApp/Presenter/OrderPresenter.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace SalesWinApp.Presenter
 {
     public class OrderPresenter
     {
+        [DisplayName("Order ID")]
+        public int OrderId { get; set; }
+
+        [DisplayName("Member Name")]
+        public string MemberName { get; set; }
+
+        private DateTime orderDate;
+        [DisplayName("Order Date")]
+        public DateTime OrderDate
+        {
+            get => orderDate.Date;
+            set => orderDate = value;
+        }
+
+        private decimal orderTotal;
+        [DisplayName("Order Total")]
+        public decimal OrderTotal
+        {
+            get => Math.Round(orderTotal, 2);
+            set => orderTotal = value;
+        }
     }
 }
--- SalesWinApp/Presenter/ProductPresenter.cs
+++ SalesWinApp/Presenter/ProductPresenter.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace SalesWinApp.Presenter
 {
     public class ProductPresenter
     {
+        [DisplayName("Product ID")]
+        public int ProductId { get; set; }
+
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+
+        [DisplayName("Category")]
+        public string CategoryName { get; set; }
+
+        [DisplayName("Weight")]
+        public string Weight { get; set; }
+
+        [DisplayName("Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+        [DisplayName("Units In Stock")]
+        public int UnitsInStock { get; set; }
     }
 }
