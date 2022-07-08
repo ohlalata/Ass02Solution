11e174322786f9cdeeb5d5caa62877ddb691c6aa
An <tannbnse150170@fpt.edu.vn>
2022-07-08 22:53:34 +0700

    Add OrderDetail - DataAccess

--- DataAccess/Repository/OrderDetailRepository/IOrderDetailRepository.cs
+++ DataAccess/Repository/OrderDetailRepository/IOrderDetailRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace DataAccess.Repository.OrderDetailRepository
+﻿using BusinessObject;
+
+namespace DataAccess.Repository.OrderDetailRepo
 {
     public interface IOrderDetailRepository
     {
+        public void AddOrderDetail(OrderDetail orderDetail);
+        public decimal GetOrderTotal(int orderId);
+        public IEnumerable<OrderDetail> GetOrderDetails(int orderId);
+        public void DeleteOrderDetails(int orderId);
+        public void DeleteByProduct(int productId);
     }
 }
