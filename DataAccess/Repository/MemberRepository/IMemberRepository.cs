a37c6d346f62e53121f7bdb0f26a70bda06be9d0
HuDery <duyhl0211@gmail.com>
2022-07-08 23:11:41 +0700

    Add class IMemberRepository.cs and IMemberRepository.cs in DataAccess

--- DataAccess/Repository/MemberRepository/IMemberRepository.cs
+++ DataAccess/Repository/MemberRepository/IMemberRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace DataAccess.Repository.MemberRepository
+﻿using BusinessObject;
+
+namespace DataAccess.Repository.MemberRepository
 {
-    public class IMemberRepository
+    public interface IMemberRepository
     {
+        public IEnumerable<Member> GetMembersList();
+        public int GetNextMemberId();
+        public bool IsAdminLogin(string email, string password);
+        public Member Login(string email, string password);
+        public void AddMember(Member member);
+        public void UpdateMember(Member member);
+        public void DeleteMember(int memberId);
+        public Member GetMember(int memberId);
+        public Member GetMember(string email);
+        public IEnumerable<Member> SearchMember(string name);
+        public IEnumerable<Member> SearchMemberByCountry(string country, IEnumerable<Member> searchList);
+        public IEnumerable<Member> SearchMemberByCity(string country, string city, IEnumerable<Member> searchList);
     }
 }
--- DataAccess/Repository/MemberRepository/MemberRepository.cs
+++ DataAccess/Repository/MemberRepository/MemberRepository.cs
@@ -1,6 +1,52 @@
-﻿namespace DataAccess.Repository.MemberRepository
+﻿using BusinessObject;
+using DataAccess.DAO;
+
+namespace DataAccess.Repository.MemberRepository
 {
     public class MemberRepository
     {
+        public bool IsAdminLogin(string email, string password) => MemberDAO.GetMemberDao.IsAdminLogin(email, password);
+        public void AddMember(Member member) => MemberDAO.GetMemberDao.AddMember(member);
+
+        public void DeleteMember(int memberId) => MemberDAO.GetMemberDao.Delete(memberId);
+
+        public IEnumerable<Member> GetMembersList()
+        {
+            return MemberDAO.GetMemberDao.GetMembersList();
+        }
+
+        public IEnumerable<Member> SearchMember(string name)
+        {
+            return MemberDAO.GetMemberDao.SearchMember(name);
+        }
+
+        public Member Login(string email, string password)
+        {
+            return MemberDAO.GetMemberDao.Login(email, password);
+        }
+
+        public void UpdateMember(Member member) => MemberDAO.GetMemberDao.Update(member);
+
+        public IEnumerable<Member> SearchMemberByCountry(string country, IEnumerable<Member> searchList)
+        {
+            return MemberDAO.GetMemberDao.FilterMemberByCountry(country, searchList);
+        }
+
+        public IEnumerable<Member> SearchMemberByCity(string country, string city, IEnumerable<Member> searchList)
+        {
+            return MemberDAO.GetMemberDao.FilterMemberByCity(country, city, searchList);
+        }
+
+        public Member GetMember(int memberId)
+        {
+            return MemberDAO.GetMemberDao.GetMemberByMemberId(memberId);
+        }
+
+        public int GetNextMemberId()
+        {
+            return MemberDAO.GetMemberDao.GetNextMemberId();
+        }
+
+        public Member GetMember(string email) => MemberDAO.GetMemberDao.GetMemberByEmail(email);
     }
 }
